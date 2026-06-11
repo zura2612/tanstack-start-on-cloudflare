3372d690c2685a779619ebe37a2b80dfdacf065f
Francois Vauchot <98938679+zura2612@users.noreply.github.com>
2026-06-11 17:08:17 +0200

    Enable devtools and cloudflare plugins in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite'
-//import { devtools } from '@tanstack/devtools-vite'
+import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { nitro } from 'nitro/vite'
-//import { cloudflare } from '@cloudflare/vite-plugin'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
- // resolve: { tsconfigPaths: true },
+  resolve: { tsconfigPaths: true },
   plugins: [
- //   devtools(),
- //   cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    viteTsConfigPaths({ projects:["./tsconfig.json"] }),
+    devtools(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart(),
-    nitro(),
     viteReact(),
   ],
 })
