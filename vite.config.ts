6472f2ce540303f44384571554b15a1a118a571f
Francois Vauchot <98938679+zura2612@users.noreply.github.com>
2026-06-11 07:32:08 +0200

    Disable Cloudflare plugin in Vite config
    Comment out the Cloudflare plugin configuration in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
+//import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+ //   cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
