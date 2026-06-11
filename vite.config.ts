f28c38389eda30bd6b4351a8838c5c6a7f94f88d
Francois Vauchot <98938679+zura2612@users.noreply.github.com>
2026-06-11 08:07:08 +0200

    Refactor vite.config.ts ajout nitro et vitsTsConfigPaths
    Commented out devtools and cloudflare plugins, and adjusted tsconfigPaths resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
+//import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import { nitro } from 'nitro/vite'
 //import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
+ // resolve: { tsconfigPaths: true },
   plugins: [
-    devtools(),
+ //   devtools(),
  //   cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    viteTsConfigPaths({ projects:["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
